60dd7a506ef4dc94bb7b6d03762b0c90252e20bf
Marzio95 <marziodellarocca14@gmail.com>
2022-04-07 15:47:42 +0200

    inserisco la bad_word tramite il _GET dell'utente

--- index.php
+++ index.php
@@ -8,30 +8,14 @@
 </head>
 <body>
 <?php
-    $text = 'Roma Roma Roma,
-    core de stà città,
-    unico grande amore,
-    de tanta e tanta gente,
-    che fai sospirà.
-    Roma Roma Roma,
-    lassace cantà,
-    da stà voce nasce un core,
-    so centomila voci che hai fatto nammorà.
-    Roma Roma bella,
-    t’ho dipinta io, 
-    gialla come er sole,
-    rossa come er core mio.
-    Roma Roma mia,
-    nun te fa cantà,
-    tu sei nata grande,  
-    e grande hai da restà.    
-    Roma Roma Roma,    
-    core de stà città,    
-    unico grande amore,    
-    de tanta e tanta gente,
-    ch’hai fatto nammorà.';
+    $text = 'Roma Roma Roma,core de stà città,unico grande amore,de tanta e tanta gente,che fai sospirà.Roma Roma Roma,lassace cantà,da stà voce nasce un core,so centomila voci che hai fatto nammorà.Roma Roma bella,t’ho dipinta io, gialla come er sole,rossa come er core mio.Roma Roma mia,nun te fa cantà,tu sei nata grande,  e grande hai da restà.    Roma Roma Roma,    core de stà città,    unico grande amore,    de tanta e tanta gente,ch’hai fatto nammorà.';
+    $bad_word;
+    $bad_word = $_GET['word'];
     ?>
 </body>
 <h2> <?= $text; ?> </h2>
 <h2>La lunghezza del testo è: <?= strlen($text) ?> </h2>
+<h2> <?= str_replace( $bad_word, '***',$text) ?> </h2>
+<h2>La lunghezza del nuovo testo è: <?= strlen(str_replace( $bad_word, '***',$text)) ?></h2>
+<h2>La bad word è scelta inserendo nel link ?word= <?= $bad_word ?> (questa è l attuale bad word) </h2>
 </html>
